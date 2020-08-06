8b1790197c08b05911240626f4869942da7abc73
codebuff2020 <codebuff2020@gmail.com>
2020-08-06 16:24:51 -0700

    added logger

--- src/main/java/com/udemy/spring/boot/customer/democrud/rest/EmployeeRestController.java
+++ src/main/java/com/udemy/spring/boot/customer/democrud/rest/EmployeeRestController.java
@@ -1,7 +1,10 @@
 package com.udemy.spring.boot.customer.democrud.rest;
 
+import com.udemy.spring.boot.customer.democrud.DemocrudApplication;
 import com.udemy.spring.boot.customer.democrud.dao.EmployeeDao;
 import com.udemy.spring.boot.customer.democrud.entity.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +14,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-public class EmployeeRestController  {
-
+public class EmployeeRestController {
+    Logger logger = LoggerFactory.getLogger(EmployeeRestController.class);
     private EmployeeDao employeeDao;
 
     @Autowired
-    public EmployeeRestController(EmployeeDao theEmployeeDao)
-    {
+    public EmployeeRestController(EmployeeDao theEmployeeDao) {
         employeeDao = theEmployeeDao;
     }
 
     @GetMapping("/employees")
-    public List<Employee> getEmployees(){
-       return employeeDao.findAll();
+    public List<Employee> getEmployees() {
+        logger.info("retrieving all employees data");
+        return employeeDao.findAll();
     }
 }
